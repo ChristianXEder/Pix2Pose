2134dca0d2b4c957802a548135d3963fc13bde72
Christian Eder <christianxeder@gmail.com>
2024-01-04 14:04:50 +0000

    remove files from PeterH, add train file

---
+++ train_deform.sh
@@ -0,0 +1,40 @@
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 1 1.0
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 405 1.0
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 809 1.0
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 1213 1.0
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 1617 1.0
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 2021 1.0
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 2425 1.0
+
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 1 0.8
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 405 0.8
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 809 0.8
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 1213 0.8
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 1617 0.8
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 2021 0.8
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 2425 0.8
+
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 1 0.6
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 405 0.6
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 809 0.6
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 1213 0.6
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 1617 0.6
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 2021 0.6
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 2425 0.6
+
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 1 0.4
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 405 0.4
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 809 0.4
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 1213 0.4
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 1617 0.4
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 2021 0.4
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 2425 0.4
+
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 1 0.2
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 405 0.2
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 809 0.2
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 1213 0.2
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 1617 0.2
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 2021 0.2
+python3 tools/3_train_pix2pose_wo_background_crop_deform.py cfg/deform.json deform_dataset 2425 0.2
+
